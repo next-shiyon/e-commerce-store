b6d7c13cee5ea8d89498e148230f8cc799ba83ae
next-shiyon <4.man.ach@gmail.com>
2023-08-18 11:46:15 +0900

    feat: 상품 리스트 firebase DB 에서 가져오기

--- src/components/parts/product/ProductList.tsx
+++ src/components/parts/product/ProductList.tsx
@@ -1,103 +1,37 @@
+import { useEffect, useState } from "react";
 import { ProductCard } from "./ProductCard";
-
-const testData = [
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "700",
-    title: "마르지엘라 지갑1",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑2",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑2",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑3",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑4",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑4",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑4",
-  },
-  {
-    category: "지갑",
-    createDate: "8/17/2023",
-    description: "마르지엘라 지갑이다.",
-    id: "66d1335f-f5c3-4dfa-901d-7fc2c3fa80f7",
-    image:
-      "https://res.cloudinary.com/ddwqzra81/image/upload/v1692262854/db7it7i3imunxrnztv8x.jpg",
-    option: "s, m, lg",
-    price: "7000",
-    title: "마르지엘라 지갑4",
-  },
-];
+import { getDatabase, onValue, ref } from "firebase/database";
+import { ProductDto } from "../../../assets/types/ProductDto";
 
 export const ProductList = () => {
+  const [productList, setProductList] = useState<ProductDto[]>();
+
+  useEffect(() => {
+    const db = getDatabase();
+    const productRef = ref(db, "products/");
+    onValue(productRef, (snapshop) => {
+      const data = snapshop.val();
+
+      const newDataArray = [];
+
+      for (const key in data) {
+        const newData = {
+          ...data[key],
+        };
+        newDataArray.push(newData);
+      }
+
+      setProductList(newDataArray);
+    });
+  }, []);
+
   return (
     <>
       <div className="grid place-items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {testData.map((product) => {
-          return <ProductCard key={product.id} product={product} />;
-        })}
+        {productList &&
+          productList.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
       </div>
     </>
   );
